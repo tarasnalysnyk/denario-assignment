cb38edc8bbb2f29d7e0926ada2bda7b70d3d4ff4
Ivan <ivan.nikolaichuk.upwork@gmail.com>
2021-10-14 07:44:10 +0200

    - Add pojos, task description, test

---
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -0,0 +1,13 @@
+package com.nphase.service;
+
+import com.nphase.entity.ShoppingCart;
+
+import javax.naming.OperationNotSupportedException;
+import java.math.BigDecimal;
+
+public class ShoppingCartService {
+
+    public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) throws OperationNotSupportedException {
+        throw new OperationNotSupportedException("TODO");
+    }
+}
