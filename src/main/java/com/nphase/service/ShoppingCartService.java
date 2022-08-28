8b46dc453eb6c772ef5ed61b1800df50b04e2404
tnalysnyk <nalysnyk@cngroup.dk>
2022-08-28 13:06:19 +0200

    task2: calculate total price with discount

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,24 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
 
-    public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
-        return shoppingCart.getProducts()
-                .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
-                .reduce(BigDecimal::add)
-                .orElse(BigDecimal.ZERO);
-    }
+	private final BigDecimal DISCOUNT = BigDecimal.valueOf(0.1); // can be moved to config or env param
+
+	public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
+		return shoppingCart.getProducts()
+				.stream()
+				.map(this::calculateWithDiscountIfApplicable)
+				.reduce(BigDecimal::add)
+				.orElse(BigDecimal.ZERO);
+	}
+
+	private BigDecimal calculateWithDiscountIfApplicable(Product product) {
+		BigDecimal amount = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+		return amount.subtract(product.getQuantity() > 3 ? amount.multiply(DISCOUNT) : BigDecimal.ZERO);
+	}
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -6,23 +6,32 @@ import com.nphase.entity.ShoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class ShoppingCartServiceTest {
-    private final ShoppingCartService service = new ShoppingCartService();
+	private final ShoppingCartService service = new ShoppingCartService();
 
-    @Test
-    public void calculatesPrice()  {
-        ShoppingCart cart = new ShoppingCart(Arrays.asList(
-                new Product("Tea", BigDecimal.valueOf(5.0), 2),
-                new Product("Coffee", BigDecimal.valueOf(6.5), 1)
-        ));
+	@Test
+	public void calculatesPrice() {
+		ShoppingCart cart = new ShoppingCart(Arrays.asList(
+				new Product("Tea", BigDecimal.valueOf(5.0), 2),
+				new Product("Coffee", BigDecimal.valueOf(6.5), 1)
+		));
 
-        BigDecimal result = service.calculateTotalPrice(cart);
+		BigDecimal result = service.calculateTotalPrice(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
-    }
+		Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+	}
+
+	@Test
+	public void calculatesPriceWithDiscount() {
+		ShoppingCart cart = new ShoppingCart(Arrays.asList(
+				new Product("Tea", BigDecimal.valueOf(5.0), 5),
+				new Product("Coffee", BigDecimal.valueOf(3.5), 3)
+		));
+		BigDecimal result = service.calculateTotalPrice(cart);
+		Assertions.assertEquals(new BigDecimal("33.00"), result);
+	}
 
 }
